cead95581dde3529acd608ce9104d60e3ba2cead
TTaket <575500603@qq.com>
2023-10-25 17:41:07 +0800

    feat: finish echo idl and service

---
+++ cmd/echo/Echo_client.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <random>
+#include <thrift/protocol/TBinaryProtocol.h>
+#include <thrift/transport/TSocket.h>
+#include <thrift/transport/TTransportUtils.h>
+
+#include "./echoservice/Echo.h"
+
+using namespace std;
+using namespace apache::thrift;
+using namespace apache::thrift::protocol;
+using namespace apache::thrift::transport;
+
+
+int main() {
+  std::shared_ptr<TTransport> socket(new TSocket("localhost", 9001));
+  std::shared_ptr<TTransport> transport(new TBufferedTransport(socket));
+  std::shared_ptr<TProtocol> protocol(new TBinaryProtocol(transport));
+  EchoClient client(protocol);
+
+  try {
+    transport->open();
+
+    srand(time(0));
+    Echo_SendInfo sendinfo;
+    sendinfo.id = rand()%100;
+    sendinfo.info = "你好 _ hello server";
+
+    Echo_RecvInfo recvinfo;
+    client.Echo_Send(recvinfo, sendinfo);
+    cout <<"现在服务器时间: " << recvinfo.time <<endl;
+    cout <<recvinfo.id <<" " << recvinfo.info <<endl;
+
+    transport->close();
+  } catch (TException& tx) {
+    cout << "ERROR: " << tx.what() << endl;
+  }
+}
