c6499779623edd98a49eb1a35e84b69a4f8021f3
TTaket <575500603@qq.com>
2023-11-02 21:44:24 +0800

    feat: add video test

--- cmd/web/rpc.hh
+++ cmd/web/rpc.hh
@@ -1,7 +1,11 @@
 #pragma once
 #include "../echo/echoservice/Echo.h"
+#include "../video/videoservice/Video.h"
 //rpc.h 包含所有微服务的定义
 namespace rpc{
     //处理函数
     std::string  Del_echo(const std::string &s);
+    std::string  VideoUpload(const std::string &s);
+    std::string  VideoDownload(const std::string &s);
+    void  VideoClean(const std::string &s);
 };
